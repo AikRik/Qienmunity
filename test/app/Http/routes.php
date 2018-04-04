c440c4f751749b0227031cdc5918b11c578eedd6
Steven Visser <sevisser1@gmail.com>
2018-04-04 11:51:21 +0200

    alles werkt

---
+++ test/app/Http/routes.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/community', function () {
+    return view('community');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/nieuws', function () {
+    return view('nieuws');
+});
+
+Route::get('/resources', function () {
+    return view('resources');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+
+});
+
+Route::get('/verify', function () {
+    return "testing posting";
+
+});
+
---
+++ test/resources/views/community.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        Community<br><br>
+
+        <a href="http://localhost:8000/">Home</a><br>
+        <a href="http://localhost:8000/dashboard">Dashboard</a><br>
+        <a href="http://localhost:8000/nieuws">Nieuws</a><br>
+        <a href="http://localhost:8000/contact">Contact</a><br>
+        <a href="http://localhost:8000/resources">Resources</a><br>
+    </body>
+</html>
